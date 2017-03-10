74290cc23f090c9ae7f90ada7cab3902151b3b89
Chris Badahdah <a.s.badahdah@gmail.com>
2017-03-10 05:26:56 -0700

    Reorganized tests and added new test.

---
+++ test/suite/moderate/range.scm
@@ -0,0 +1,19 @@
+(define reverse (lambda (ls)
+  (begin
+    (define reverse-inner (lambda (ls acc)
+      (if (null? ls)
+        acc
+        (reverse-inner (cdr ls) (cons (car ls) acc)))))
+    (reverse-inner ls '()))))
+
+(define inc (lambda (x) (+ x 1)))
+
+(define range (lambda (n)
+  (begin
+    (define range-iter (lambda (ls i)
+      (if (= i n)
+        ls
+        (range-iter (cons i ls) (inc i)))))
+    (reverse (range-iter '() 0)))))
+
+(write (range 10))
