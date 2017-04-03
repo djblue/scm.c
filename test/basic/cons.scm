8daec04193e20893e64ec2f2da1b0a910e2c35f2
Chris Badahdah <a.s.badahdah@gmail.com>
2017-04-02 21:10:01 -0700

    Added tests for `set-car!` and `set-cdr!`.

--- test/basic/cons.scm
+++ test/basic/cons.scm
@@ -5,3 +5,13 @@
 (write (pair? '(1 . 2)))
 (write (car '(1 . 2)))
 (write (cdr '(1 . 2)))
+
+(define p (cons '() '()))
+
+(write (car p))
+(set-car! p 1)
+(write (car p))
+
+(write (cdr p))
+(set-cdr! p 2)
+(write (cdr p))
