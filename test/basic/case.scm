ffe16573b6b654115dc06aec4687dbacb2a1c14e
Chris Badahdah <a.s.badahdah@gmail.com>
2017-04-07 17:48:19 -0700

    Added case statement.

---
+++ test/basic/case.scm
@@ -0,0 +1,19 @@
+(write (case 'hello
+         [(1) 2]
+         [else 'hello]))
+
+(write (case (+ 7 5)
+         [(1 2 3) 'small]
+         [(10 11 12) 'big]))
+
+(write (case (- 7 5)
+         [(1 2 3) 'small]
+         [(10 11 12) 'big]))
+
+(write (case (list 'y 'x)
+         [((a b) (x y)) 'forwards]
+         [((b a) (y x)) 'backwards]))
+
+(write (case 'x
+         [(x) "ex"]
+         [('x) "quoted ex"]))
