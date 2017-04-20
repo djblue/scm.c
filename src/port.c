916fb98bb52686d73ef1f36b554f686d78ffe74f
Chris Badahdah <a.s.badahdah@gmail.com>
2017-04-20 07:10:42 -0700

    Fixed issue with recursive `load`.

--- src/port.c
+++ src/port.c
@@ -34,19 +34,30 @@ object_t scm_open(vm_t *vm, object_t args) {
   object_t str = car(vm, args);
 
   if (false(string(str)))
-    return make_error(vm, "First argument is not a string.", str);
+    return make_error(vm, "open: first argument is not a string.", str);
 
   char *path = string_cstr(str);
   FILE *fp = fopen(path, "r");
 
   if (fp == NULL) {
-    return make_error(vm, "Can't open input file.", str);
+    return make_error(vm, "open: can't open input file.", str);
   }
 
   object_t port = make_port(vm);
   object_data(port, port_t).path = str;
   object_data(port, port_t).fp = fp;
   return port;
+}
+
+object_t scm_close(vm_t *vm, object_t args) {
+  object_t port = (scm_type(args) == PORT) ? args : car(vm, args);
+
+  if (scm_type(port) != PORT)
+    return make_error(vm, "close: argument is not a port", port);
+
+  fclose(port_pointer(args));
+
+  return t;
 }
 
 void define_port(vm_t *vm, object_t env) {
--- src/port.h
+++ src/port.h
@@ -11,6 +11,7 @@ extern object_t ueof;
 
 object_t make_port_from_file(vm_t *vm, FILE *fp);
 object_t scm_open(vm_t *vm, object_t args);
+object_t scm_close(vm_t *vm, object_t args);
 
 FILE *port_pointer(object_t port);
 void print_port(vm_t *vm, object_t port);
