6a8764d7ea92a30ac8b4ddc8c740778296230e30
Chris Badahdah <a.s.badahdah@gmail.com>
2017-04-23 00:43:42 -0700

    Moved number functions into `number.c`.

--- src/number.h
+++ src/number.h
@@ -5,22 +5,14 @@
 #include "vm.h"
 
 object_t make_fixnum(vm_t *vm, char *str);
-object_t make_fixnum_int(vm_t *vm, long fix);
 object_t make_flonum(vm_t *vm, char *str);
-object_t make_flonum_float(vm_t *vm, float flo);
 
 long scm_fixnum(object_t o);
 int scm_is_fixnum(object_t o);
 
-object_t fixnum(object_t o);
-object_t flonum(object_t o);
-object_t number(object_t o);
-
-object_t plus(vm_t *vm, object_t a, object_t b);
-object_t minus(vm_t *vm, object_t a, object_t b);
-object_t multiply(vm_t *vm, object_t a, object_t b);
-
 object_t number_eq(vm_t *vm, object_t a, object_t b);
+
+void define_number(vm_t *vm, object_t env);
 
 #endif
 
